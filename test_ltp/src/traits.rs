a41fbc173d2280cb089726ec95a6519db6b9fee6
Karl-D-Asmussen <karl.d.asmussen@gmail.com>
2017-06-19 11:24:36 +0200

    Mon Jun 19 11:24:36 CEST 2017

--- test_ltp/src/traits.rs
+++ test_ltp/src/traits.rs
@@ -4,19 +4,56 @@ use ltp::*;
 
 trait Array {
     type Element;
+
+    type Shape : Array<Element = usize>;
+    type Flat : Array<Element = Self::Element>;
+
     fn rank(&self) -> usize;
     fn len(&self) -> usize;
 }
 
-trait ArrayRef<'a> : Array + LTP<'b> {
-    type Entry : LTP<'static> + Deref<Target = Self::Element>;
-    fn at_ref<'a, I>(&'a self, coord : I) -> <Self::Entry as LTP<'static>>::X
+trait ArrayRef {
+    type Owned : Array;
+
+    type Entry : LTP<'static> + Deref<Target = <Self::Owned as Array>::Element>;
+    fn at_ref<'a, IR, I>(&'a self, coord : I) -> <Self::Entry as LTP<'static>>::X
         where Self::Entry : LT<R = &'a ()>,
-              I : Array<Element = usize>;
+              IR : ArrayRef<Owned = I>,
+              I : Array<Element = isize>;
+    
+    type ShapeRef : LTP<'static> + ArrayRef;
+    fn shape<'a>(&'a self) -> <Self::ShapeRef as LTP<'static>>::X
+        where Self::ShapeRef : LT<R = &'a ()>,
+              <Self::ShapeRef as ArrayRef>::Owned : Array<Element = usize>;
 
-    type Shape : ArrayRef<'b> + <Element = usize>;
-    fn shape<'a>(&'a self) -> <Self::Shape as LTP<'static>>::X where Self::Shape : LT<R = &'a ()>;
+    type FlatRef : LTP<'static> + ArrayRef;
+    fn flat<'a>(&'a self) -> <Self::FlatRef as LTP<'static>>::X
+        where Self::FlatRef : LT<R = &'a ()>,
+              <Self::FlatRef as ArrayRef>::Owned : Array<Element = <Self::Owned as Array>::Element>;
+}
 
-    type Flat : ArrayRef<'b> + Array<Element = Self::Element>;
-    fn flat<'a>(&'a self) -> <Self::Flat as LTP<'static>>::X where Self::Flat : LT<R = &'a ()>;
+trait ArrayMut : ArrayRef {
+    type EntryMut : LTP<'static> + Deref<Target = <Self::Owned as Array>::Element>;
+    fn at_mut<'a, IR, I>(&'a self, coord : I) -> <Self::EntryMut as LTP<'static>>::X
+        where Self::Entry : LT<R = &'a ()>,
+              IR : ArrayRef<Owned = I>,
+              I : Array<Element = isize>;
+}
+
+
+trait ArrayInto : Array {
+}
+
+struct Ston(usize);
+impl Array for Ston {
+    type Element = usize;
+    type Shape = Ston;
+    type Flat = Ston;
+
+    fn rank(&self) -> usize { 1 }
+    fn len(&self) -> usize { 1 }
+}
+
+impl<'a> ArrayRef for It<'a, Ston> {
+    type Entry = It<'static, usize>;
 }
